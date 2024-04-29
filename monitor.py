1cc5e932ec5547bae92faf6563533c4ffa9ca2f0
zyx1121 <yongxiang.zhan@outlook.com>
2024-04-29 10:43:25 +0800

    Refactor serial monitor

--- monitor.py
+++ monitor.py
@@ -5,15 +5,24 @@ import queue
 import serial
 import os
 import serial.tools.list_ports
-from rich.console import Console
-from rich.panel import Panel
 
-console = Console()
+
+COLOR_NONE = '\033[0m'
+COLOR_ERROR = '\033[91m'
+COLOR_WHITE = '\033[97m'
+STYLE_BOLD = '\033[1m'
+CURSOR_SAVE = '\033[s'
+CURSOR_RESTORE = '\033[u'
+CURSOR_HOME = '\033[1000D'
+CURSOR_CLEAR = '\033[K'
 
 class SerialMonitor():
+    DEFAULT_BAUDRATE = 115200
+    DEFAULT_TIMEOUT = 5
+
     def __init__(self):
         self.port = None
-        self.baudrate = 115200
+        self.baudrate = self.DEFAULT_BAUDRATE
         self.parity = serial.PARITY_NONE
 
         self.input_buffer = ''
@@ -23,8 +32,22 @@ class SerialMonitor():
         self.input_queue = queue.Queue()
         self.stop_queue = queue.Queue()
 
-        self.input_thread = threading.Thread(target=self.add_input, args=(self.input_queue,self.stop_queue,))
+        self.input_thread = threading.Thread(target=self.add_input, args=(self.input_queue, self.stop_queue))
         self.input_thread.daemon = True
+
+
+    @staticmethod
+    def handle_input_error(error_msg):
+        def decorator(func):
+            def wrapper(self, *args, **kwargs):
+                while True:
+                    try:
+                        return func(self, *args, **kwargs)
+                    except (KeyError, ValueError):
+                        self.init_screen(error_msg)
+            return wrapper
+        return decorator
+
 
     def getch(self):
         try:
@@ -44,18 +67,20 @@ class SerialMonitor():
                 termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
             return ch.encode()
 
+
     def add_input(self, input_queue, stop_queue):
         while True:
             input_char = self.getch()
             input_queue.put(input_char)
 
-    def init_screen(self):
-        self.clear_screen()
-        console.print(Panel("Serial Monitor", width=64, padding=(1,8)), style="bold", justify="center")
-        console.print()
+
+    def init_screen(self, error_msg):
+        self.clear_console()
+        print(COLOR_WHITE + STYLE_BOLD + 'Hex Serial Monitor ' + COLOR_NONE + COLOR_ERROR + error_msg + COLOR_NONE + '\n')
+
 
     def get_timestamp(self):
-        return '[' + datetime.datetime.now().strftime("%H:%M:%S") + '] '
+        return '[' + datetime.datetime.now().strftime('%H:%M:%S') + '] '
 
 
     def get_ports(self):
@@ -64,136 +89,131 @@ class SerialMonitor():
 
         for i, port in enumerate(ports):
             port_dict[i] = port.device
-            console.print(f"- [{i}] {port.device}")
-        console.print()
+            print(f'- [{i}] {port.device} ({port.description})')
+        print()
 
         return port_dict
 
+
+    @handle_input_error('(Invalid port number. Please try again.)')
     def select_port(self):
-        while True:
-            try:
-                self.init_screen()
-                port_dict = self.get_ports()
-                selected_index = int(input("Enter the port number to connect: "))
-                selected_port = port_dict[selected_index]
-                return selected_port
-            except (ValueError, KeyError):
-                console.print("Invalid port number. Please try again.", style="bold red")
+        port_dict = self.get_ports()
+        selected_index = int(input('Enter the port number to connect: '))
+        selected_port = port_dict[selected_index]
+        self.init_screen('')
+        return selected_port
 
+
+    @handle_input_error('(Invalid baudrate. Please try again.)')
     def select_baudrate(self):
-        while True:
-            try:
-                self.init_screen()
-                baudrate = int(input("Enter the baudrate: "))
-                return baudrate
-            except ValueError:
-                console.print("\033[91m" + "Invalid baudrate. Please try again.\033[0m\n")
+        baudrate = input(f'Enter the baudrate (default {self.DEFAULT_BAUDRATE}): ')
+        if not baudrate:
+            baudrate = self.DEFAULT_BAUDRATE
+        self.init_screen('')
+        return int(baudrate)
 
+
+    @handle_input_error('(Invalid parity. Please try again.)')
     def select_parity(self):
-        while True:
-            try:
-                self.init_screen()
-                parity = input("Enter the parity (N, E, O): ").upper()
-                if parity == "N":
-                    return serial.PARITY_NONE
-                elif parity == "E":
-                    return serial.PARITY_EVEN
-                elif parity == "O":
-                    return serial.PARITY_ODD
-                else:
-                    console.print("Invalid parity. Please try again.", style="bold red")
-            except ValueError:
-                console.print("Invalid parity. Please try again.", style="bold red")
+        parity = input('Enter the parity (N, E, O) (default N): ').upper()
+        if parity == 'N' or not parity:
+            return serial.PARITY_NONE
+        elif parity == 'E':
+            return serial.PARITY_EVEN
+        elif parity == 'O':
+            return serial.PARITY_ODD
+        else:
+            raise ValueError
+
 
     def open_port(self):
         try:
-            self.ser = serial.Serial(port=self.port, baudrate=self.baudrate, parity=self.parity, timeout=5)
-            self.ser.flushInput()
-            self.ser.flushOutput()
+            self.serial = serial.Serial(port=self.port, baudrate=self.baudrate, parity=self.parity, timeout=self.DEFAULT_TIMEOUT)
+            self.serial.flushInput()
+            self.serial.flushOutput()
         except serial.SerialException:
-            console.print("Failed to open the selected port. Please try again.", style="bold red")
+            print('\n\n' + COLOR_ERROR + 'Monitor: Failed to open the selected port. Exiting now...' + COLOR_NONE + '\n')
+            exit(1)
+
 
     def get_info(self):
-        return f"Port: {self.port} | Baudrate: {self.baudrate} | Parity: {self.parity} | RX: {self.total_bytes_rx} | TX: {self.total_bytes_tx}"
+        return f'Port: {self.port} | Baudrate: {self.baudrate} | Parity: {self.parity} | RX: {self.total_bytes_rx} | TX: {self.total_bytes_tx}'
 
-    def clear_screen(self):
+
+    def clear_console(self):
         if os.name == 'nt':
             os.system('cls')
         else:
             os.system('clear')
 
-    def process_input(self, status):
+
+    def process_input(self):
         if not self.input_queue.empty():
             keyboard_input = self.input_queue.get().upper()
 
+            # Check if the input is a valid hex character
             if all(b in b'0123456789ABCDEF' for b in keyboard_input):
                 self.input_buffer += keyboard_input.decode()
+                print(CURSOR_SAVE + '\n\n' + CURSOR_HOME + CURSOR_CLEAR + self.input_buffer + CURSOR_RESTORE, end='', flush=True)
 
-                if status:
-                    status.update('[bold blue]' + self.input_buffer)
-
+            # Check if the input is a valid hex string
             if keyboard_input == b'\r':
+                print(CURSOR_SAVE + '\n\n' + CURSOR_HOME + CURSOR_CLEAR + '' + CURSOR_RESTORE, end='', flush=True)
                 if len(self.input_buffer) == 0:
-                    console.print('\n\n' + self.get_timestamp() + self.get_info() + '\n', style='blue')
+                    print('\n\n' + self.get_timestamp() + self.get_info() + '\n')
                 elif len(self.input_buffer) % 2 == 0:
-                    console.print('\n' + self.get_timestamp() + self.input_buffer + '\n', style='blue')
+                    print('\n' + self.get_timestamp() + self.input_buffer + '\n')
                     send_bytes = bytes.fromhex(self.input_buffer)
 
-                    self.ser.write(send_bytes)
+                    self.serial.write(send_bytes)
                     self.input_buffer = ''
 
-            if keyboard_input == b'\x7f':
+            # Check if the input is a backspace
+            if keyboard_input == b'\x7f' or keyboard_input == b'\x08':
                 self.input_buffer = self.input_buffer[:-1]
+                print(CURSOR_SAVE + '\n\n' + CURSOR_HOME + CURSOR_CLEAR + self.input_buffer + CURSOR_RESTORE, end='', flush=True)
 
-                if status:
-                    status.update('[bold blue]' + self.input_buffer)
-
+            # Check if the input is a CTRL+C
             if keyboard_input == b'\x03':
                 raise KeyboardInterrupt()
 
-    def process_output(self, status):
-        try:
-            bytes_to_read = self.ser.inWaiting()
-            while bytes_to_read:
-                data = self.ser.read()
-                bytes_to_read = bytes_to_read - 1
-                console.print(data.hex(), end='', style='green')
-                self.total_bytes_rx += len(data)
 
+    def process_output(self):
+        try:
+            bytes_to_read = self.serial.inWaiting()
+            while bytes_to_read:
+                data = self.serial.read(bytes_to_read)
+                self.total_bytes_rx += bytes_to_read
+                bytes_to_read = 0
+                print(data.hex(' '), end=' ', style='green')
         except IOError:
             raise IOError()
 
 
     def run(self):
+        self.init_screen('')
+
         self.port = self.select_port()
         self.baudrate = self.select_baudrate()
         self.parity = self.select_parity()
 
+        self.clear_console()
+
         self.open_port()
 
         self.input_thread.start()
-        self.clear_screen()
 
         while True:
-            if self.input_buffer:
-                with console.status('[bold blue]' + self.input_buffer, spinner='arc', spinner_style='blue') as status:
-                    while True:
-                        self.process_input(status)
-                        self.process_output(status)
-                        if not self.input_buffer:
-                            break
-            else:
-                while True:
-                    self.process_input(None)
-                    self.process_output(None)
-                    if self.input_buffer:
-                        break
+            self.process_input()
+            self.process_output()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     try:
         SerialMonitor().run()
     except IOError:
-        console.print("\n\nMonitor: Disconnected (I/O Error)", style='bold red')
+        print('\n\n' + COLOR_ERROR + 'Monitor: Disconnected (I/O Error)' + COLOR_NONE + '\n')
+        exit(1)
     except KeyboardInterrupt:
-        console.print("\n\nMonitor: Exiting Now...", style='bold red')
-        sys.exit(1)
+        print('\n\n' + COLOR_ERROR + 'Monitor: Exiting Now...' + COLOR_NONE + '\n')
+        exit(1)
